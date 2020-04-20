7bc639928483c6e604c9b45d34773dc5f5f2c31b
Francisco Giovanildo Teixeira de Souza <giovanildos@gmail.com>
2020-04-20 03:17:02 -0300

    separando código em métodos e classes

--- src/main/java/com/giovanildo/jpa/service/ui/Main.java
+++ src/main/java/com/giovanildo/jpa/service/ui/Main.java
@@ -6,7 +6,6 @@ import java.util.List;
 import com.giovanildo.jpa.service.interfaces.CrudService;
 import com.giovanildo.models.Clube;
 import com.giovanildo.models.Competidor;
-import com.giovanildo.models.CompetidorEmCampo;
 import com.giovanildo.models.EAtleta;
 import com.giovanildo.models.Partida;
 import com.giovanildo.models.Torneio;
@@ -14,38 +13,49 @@ import com.giovanildo.services.impl.ClubeService;
 import com.giovanildo.services.impl.EatletaService;
 import com.giovanildo.services.impl.PartidaService;
 import com.giovanildo.services.impl.TorneioService;
+import com.giovanildo.utils.Controller;
 
 public class Main {
+
+	public static CrudService<Torneio, Integer> torneioService = new TorneioService();
+	public static CrudService<Partida, Integer> partidaService = new PartidaService();
+	public static CrudService<EAtleta, Integer> eaService = new EatletaService();
+	public static CrudService<Clube, Integer> clubeService = new ClubeService();
+
 	public static void main(String[] args) {
 		System.out.println("Testando JPA - competidor");
 
-		// instancia os objetos a serem persistidos e altera-os
-		// instância um Crud Service
-		// faz um foreach mostrando o conteudo da tabela
+		adicionarEatletas();
+		adicionarClubes();
+		List<Competidor> competidores = adicionarTorneios();
 
-		CrudService<EAtleta, Integer> eaService = new EatletaService();
-		CrudService<Clube, Integer> clubeService = new ClubeService();
-		CrudService<Torneio, Integer> torneioService = new TorneioService();
-		CrudService<Partida, Integer> partidaService = new PartidaService();
+		System.out.println(" --------------- Listando Dados ---------------------");
 
-		// instancia os objetos a serem persistidos e altera-os
-		// instância um Crud Service
-		// faz um foreach mostrando o conteudo da tabela
+		listandoDados();
 
-		// adicionando clubes
+		for (Partida daVez : Controller.geraPartidas(competidores)) {
+			partidaService.insert(daVez);
+		}
 
-		clubeService.insert(new Clube("Borrusia", "Alemanha"));
-		clubeService.insert(new Clube("Real", "Espanha"));
-		clubeService.insert(new Clube("Manchester", "Inglaterra"));
-		clubeService.insert(new Clube("Porto", "Portugal"));
+		System.out.println("Inserido com sucesso!");
 
-		// adicionando Eatletas
+	}
 
-		eaService.insert(new EAtleta("Giovanildo"));
-		eaService.insert(new EAtleta("Genilson"));
-		eaService.insert(new EAtleta("Fabiano"));
-		eaService.insert(new EAtleta("Zaldir"));
+	private static void listandoDados() {
+		for (Clube daVez : clubeService.all()) {
+			System.out.println(daVez.toString());
+		}
 
+		for (EAtleta daVez : eaService.all()) {
+			System.out.println(daVez.toString());
+		}
+
+		for (Torneio daVez : torneioService.all()) {
+			System.out.println(daVez.toString());
+		}
+	}
+
+	private static List<Competidor> adicionarTorneios() {
 		// adicionando Torneio/Competidores
 
 		Torneio torneio = new Torneio();
@@ -63,108 +73,22 @@ public class Main {
 
 		torneioService.insert(torneio);
 
-		System.out.println(" --------------- Listando Dados ---------------------");
+		return competidores;
+	}
 
-		for (Partida daVez : geraPartidas(competidores)) {
-			partidaService.insert(daVez);
-		};
-
-		System.out.println(" --------------- Listando Dados ---------------------");
-
-		for (Clube daVez : clubeService.all()) {
-			System.out.println(daVez.toString());
-		}
-
-		for (EAtleta daVez : eaService.all()) {
-			System.out.println(daVez.toString());
-		}
-
-		for (Torneio daVez : torneioService.all()) {
-			System.out.println(daVez.toString());
-		}
-
-		System.out.println("Inserido com sucesso!");
+	private static void adicionarEatletas() {
+		eaService.insert(new EAtleta("Giovanildo"));
+		eaService.insert(new EAtleta("Genilson"));
+		eaService.insert(new EAtleta("Fabiano"));
+		eaService.insert(new EAtleta("Zaldir"));
 
 	}
 
-	/**
-	 * gera array de partidas
-	 */
-	public static List<Partida> geraPartidas(List<Competidor> competidores) {
-		
-		List<Partida> listaPartidas = new ArrayList<Partida>();
-
-		// em caso de partidas clubes impares
-		if (competidores.size() % 2 == 1) {
-			competidores.add(0, null);
-		}
-
-		// variaveis que serao base para gerar partidas
-		int totalClubes = competidores.size();
-		int metadeClubes = totalClubes / 2;
-		Partida partida = null;
-		List<CompetidorEmCampo> competidoresEmCampo = null;
-		for (int turno = 0; turno <= 1; turno++) {
-			for (int t = 0; t < (totalClubes - 1); t++) {// for das rodadas
-				for (int m = 0; m < metadeClubes; m++) {// for dos jogos
-					// Clube está de fora nessa rodada?
-					if (competidores.get(m) == null) {
-						continue;
-					}
-					// Teste para ajustar o mando de campo
-					if (m % 2 == 1 || t % 2 == 1 && m == 0) {
-						if (turno == 0) {
-							partida = new Partida();
-							competidoresEmCampo = new ArrayList<CompetidorEmCampo>();
-							// guardando o anfitrião
-							competidoresEmCampo.add(
-									new CompetidorEmCampo(partida, competidores.get(totalClubes - m - 1), 0, true));
-							// guardando o visitante
-							competidoresEmCampo.add(new CompetidorEmCampo(partida, competidores.get(m), 0, false));
-						} else {
-							partida = new Partida();
-							competidoresEmCampo = new ArrayList<CompetidorEmCampo>();
-							// guardando o anfitrião
-							competidoresEmCampo.add(new CompetidorEmCampo(partida, competidores.get(m), 0, true));
-							// guardando o visitante
-							competidoresEmCampo.add(
-									new CompetidorEmCampo(partida, competidores.get(totalClubes - m - 1), 0, false));
-						}
-					} else {
-						
-						if (turno == 1) {
-							partida = new Partida();
-							competidoresEmCampo = new ArrayList<CompetidorEmCampo>();
-							// guardando o anfitrião
-							competidoresEmCampo.add(new CompetidorEmCampo(partida, competidores.get(m), 0, true));
-							// guardando o visitante
-							competidoresEmCampo.add(
-									new CompetidorEmCampo(partida, competidores.get(totalClubes - m - 1), 0, false));
-						} else {
-							partida = new Partida();
-							competidoresEmCampo = new ArrayList<CompetidorEmCampo>();
-							// guardando o anfitrião
-							competidoresEmCampo.add(
-									new CompetidorEmCampo(partida, competidores.get(totalClubes - m - 1), 0, true));
-							// guardando o visitante
-							competidoresEmCampo.add(new CompetidorEmCampo(partida, competidores.get(m), 0, false));
-						}
-					}
-				}
-				
-				partida.setCompetidoresEmCampo(competidoresEmCampo);
-				// inserindo na lista
-
-				listaPartidas.add(partida);
-
-				// Gira os clubes no sentido horário, mantendo o primeiro no lugar
-				Competidor remove = competidores.remove(competidores.size() - 1);
-				competidores.add(1, remove);
-			}
-		}
-
-		return listaPartidas;
-
+	private static void adicionarClubes() {
+		clubeService.insert(new Clube("Borrusia", "Alemanha"));
+		clubeService.insert(new Clube("Real", "Espanha"));
+		clubeService.insert(new Clube("Manchester", "Inglaterra"));
+		clubeService.insert(new Clube("Porto", "Portugal"));
 	}
 
 }
